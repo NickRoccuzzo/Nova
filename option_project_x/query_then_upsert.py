f2a21ab20d7c513f72bc3f0bfacad1c27da4a927
NickRoccuzzo <40014781+NickRoccuzzo@users.noreply.github.com>
2025-07-16 22:55:01 -0400

    Delete option_project_x directory

--- option_project_x/query_then_upsert.py
+++
@@ -1,9 +0,0 @@
-# query_then_upsert.py
-
-from query_options import options_dictionary, unusual_volume_report
-from upsert_options import upsert_rows, upsert_unusual_report
-
-upsert_rows(options_dictionary)
-upsert_unusual_report(unusual_volume_report)
-print("Done.")
-
